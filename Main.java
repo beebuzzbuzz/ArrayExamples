b099cc9178810d46b4d25562f063c50f28b08d2e
beebuzzbuzz <43892847+beebuzzbuzz@users.noreply.github.com>
2018-10-24 12:24:34 -0700

    task 11

--- Main.java
+++ Main.java
@@ -225,26 +225,46 @@ class Main
          * new   ar4  9 1 3 5 7
          */
          System.out.println(" ** Task 10 ** ");
+         
          System.out.println("Original ar4");
-         for (int i=0 ; i<ar4.length ; i++)
-             System.out.println(ar4[i]);
+            for (int i=0 ; i<ar4.length ; i++)
+                System.out.println(ar4[i]);
         
          int temp10=ar4[ar4.length-1];
          
-         for (int i=ar4.length-1 ; i>0 ; i--)
-            ar4[i]=ar4[i-1];
+            for (int i=ar4.length-1 ; i>0 ; i--)
+                ar4[i]=ar4[i-1];
          
          ar4[0]=temp10;
             
          System.out.println("New ar4");
-         for (int i=0 ; i<ar4.length ; i++)
-             System.out.println(ar4[i]);
+            for (int i=0 ; i<ar4.length ; i++)
+                 System.out.println(ar4[i]);
             
         /*
          * Task 11.  Reverse the order of elements in ar2
          */
+        System.out.println("** Task 11 **");
         
+        for (int i=0 ; i<ar2.length ; i++)
+            System.out.println("ar2["+i+"] = "+ar2[i]);
+            
+       
+       int kk=ar2.length-1;
+       int[] b=new int[ar2.length];
+       for (int i=0 ; i<ar2.length ; i++)
+       {
+           b[kk]=ar2[i];
+           kk--;
+        }
         
+        for (int i=0 ; i<ar2.length ; i++)
+        {
+            ar2[i]=b[i];
+        }
+               
+        for (int i=0 ; i<ar2.length ; i++)
+            System.out.println("ar2["+i+"] = "+ar2[i]);
         /*
          * Task 12: 
          * Create an array of Strings called ar5.
